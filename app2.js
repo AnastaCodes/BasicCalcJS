2cf6a9abbefb62ff424a3593256aa59191fd3158
Anastasiia Goncharova <anastagoncharova1995@gmail.com>
2024-04-25 21:29:34 +0200

    minor restructuring

--- app2.js
+++ app2.js
@@ -10,88 +10,30 @@ const action = ['-', '+', '/', '×'];
 buttons.forEach((button) => {
     button.addEventListener('click', (event) => {
         const value = event.target.innerHTML;
-        if (value === 'C') {
-            clearValues();
-            clearDisplay();
-            return;
-        }
-        if (value === '⌫') {
-            deleteNumber();
-            return;
-        }
-        if (value === '+/-') {
-            if (action.includes(display.innerHTML)) {
-                return;
-            }
-            toggle();
-            return;
-        }
-        if (value === '%') {
-            percent();
-            return;
-        }
-        //first or second number
-        if (digit.includes(value)) {
-            if (calculated) {
-                clearValues();
-                firstNumber = value;
-                displayNumber(firstNumber);
-                return;
-            }
-            if (value === '.' && display.innerHTML.includes('.')) {
-                return;
-            }
-            if (secondNumber === '' && math === '') {
-                firstNumber += value;
-                displayNumber(firstNumber);
-                return;
-            } else {
-                secondNumber += value;
-                displayNumber(secondNumber);
-            }
-            return;
-        }
-
-        if (action.includes(value)) {
-            math = value;
-            displayNumber(value);
-        }
-
-        //calculate
-        if (value === '=') {
-            if (secondNumber === '') {
-                secondNumber = firstNumber
-            }
-            switch (math) {
-                case '+':
-                    firstNumber = (+firstNumber) + (+secondNumber)
-                    break;
-                case '-':
-                    firstNumber = (+firstNumber) - (+secondNumber)
-                    break;
-                case '/':
-                    if (+secondNumber === 0) {
-                        clearValues();
-                        clearDisplay();
-                        return;
-                    }
-                    firstNumber = (+firstNumber) / (+secondNumber)
-                    break;
-                case '×':
-                    firstNumber = (+firstNumber) * (+secondNumber)
-                    break;
-            }
-            calculated = true;
-            displayNumber(firstNumber);
-        }
-
+        operation(value);
     });
 });
 
+function operation(value){
+    switch (value) {
+        case '+/-':  if(!action.includes(display.innerHTML)) { toggle();} break;
+        case 'C': clearValues();clearDisplay(); break;
+        case '⌫':  deleteNumber();break;
+        case '%':  percent();break;
+        case '=': calculate();break;
+        default:
+            if (action.includes(value)) {
+                math = value;
+                displayNumber(value);
+            }else if(digit.includes(value)){
+                fillNumber(value)
+            }
+            break;
+    }
+}
+
 function clearValues() {
-    firstNumber = '';
-    secondNumber = '';
-    math = ''
+    firstNumber = secondNumber =  math = '';
     calculated = false;
 }
 
@@ -109,20 +51,6 @@ function switchNumber(newNum) {
     } else {
         secondNumber = newNum;
     }
-}
-
-function deleteNumber() {
-    let newNum = display.innerHTML;
-    if (calculated) {
-        return;
-    }
-    if (newNum.length > 1) {
-        newNum = newNum.slice(0, -1);
-        displayNumber(newNum);
-    } else {
-        clearDisplay();
-    }
-    switchNumber(newNum);
 }
 
 function toggle() {
@@ -143,4 +71,60 @@ function percent() {
     displayNumber(newNum);
     switchNumber(newNum);
 }
+
+function fillNumber(value){
+    if (calculated) {
+        clearValues();
+        firstNumber = value;
+        displayNumber(firstNumber);
+        return;
+    }
+    if (value === '.' && display.innerHTML.includes('.')) {
+        return;
+    }
+    if (secondNumber === '' && math === '') {
+        firstNumber += value;
+        displayNumber(firstNumber);
+    } else {
+        secondNumber += value;
+        displayNumber(secondNumber);
+    }
+}
+
+function calculate(){
+    if (secondNumber === '') {
+        secondNumber = firstNumber;
+    }
+    switch (math) {
+        case '+': firstNumber = (+firstNumber) + (+secondNumber); break;
+        case '-': firstNumber = (+firstNumber) - (+secondNumber); break;
+        case '×': firstNumber = (+firstNumber) * (+secondNumber); break;
+        case '/':
+            if (+secondNumber === 0) {
+                clearValues();
+                clearDisplay();
+                return;
+            }
+            firstNumber = (+firstNumber) / (+secondNumber)
+            break;
+    }
+    calculated = true;
+    displayNumber(firstNumber);
+}
+
+
+function deleteNumber() {
+    let newNum = display.innerHTML;
+    if (calculated) {
+        return;
+    }
+    if (newNum.length > 1) {
+        newNum = newNum.slice(0, -1);
+        displayNumber(newNum);
+    } else {
+        clearDisplay();
+    }
+    switchNumber(newNum);
+}
+
 
